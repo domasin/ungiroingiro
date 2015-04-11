4f9076575dab9c8f5ab1b0cb93aa91dbbc4701e4
domasin <domenico.masini@gmail.com>
2015-04-11 14:46:11 +0200

    secondo commit

---
+++ GpxFS/GpxLoader.fs
@@ -0,0 +1,41 @@
+﻿namespace GpxFS
+
+open System
+open GpxFS.GpxXml.Functions
+open System.Collections.Generic
+
+type GpxPoint(lat:double, lon:double, ele:double, time:DateTime) = 
+    member x.Latitude = lat
+    member x.Longitude = lon
+    member x.Elevation = ele
+    member x.Time = time
+
+type GpxTrack(name:string, segs:(List<GpxPoint>)) = 
+    member x.Name = name
+    member x.Segs = segs
+
+type GpxLoader(content) = 
+
+    member this.GetTracks() = 
+        content
+        |> gpxTracks
+        |> Seq.map 
+            (fun (name,segs) -> 
+                let points = 
+                    segs
+                    |> List.map (fun (lat,lon,ele,time) -> new GpxPoint(lat,lon,ele,time))
+                new GpxTrack(name,points |> ofSeq)
+            )
+        |> ofSeq
+
+    member this.GetRoutes() = 
+        content
+        |> gpxRoutes
+        |> Seq.map 
+            (fun (name,segs) -> 
+                let points = 
+                    segs
+                    |> List.map (fun (lat,lon) -> new GpxPoint(lat,lon,0.,DateTime.MinValue))
+                new GpxTrack(name,points |> ofSeq)
+            )
+        |> ofSeq
---
+++ GpxFS/GpxXmlFunctions.fs
@@ -0,0 +1,76 @@
+﻿module GpxFS.GpxXml.Functions
+
+open System
+open System.Xml.Linq
+open System.Globalization
+
+let ofSeq (arr: 'T seq) = new System.Collections.Generic.List<'T>(arr)
+
+let gpxDoc content = XDocument.Parse(content)
+
+let gpxNameSpace = XNamespace.Get("http://www.topografix.com/GPX/1/1")
+
+let gpxWaypoints content = 
+    let gpxDoc = gpxDoc content
+    gpxDoc.Descendants(gpxNameSpace + "wpt")
+    |> Seq.map 
+        (fun wpt -> 
+            (wpt.Attribute(XName.Get("lat")).Value |> double),
+            (wpt.Attribute(XName.Get("lon")).Value |> double), 
+            (
+                let ele = wpt.Element(gpxNameSpace + "ele")
+                if ele <> null then ele.Value |> double else Double.NaN
+            ), 
+            (   
+                let name = wpt.Element(gpxNameSpace + "name")
+                if name <> null then name.Value else ""
+            ),
+            (   
+                let cmt = wpt.Element(gpxNameSpace + "cmt")
+                if cmt <> null then cmt.Value else ""
+            )
+        )
+    |> Seq.toList
+
+let gpxTracks content = 
+    let gpxDoc = gpxDoc content
+    gpxDoc.Descendants(gpxNameSpace + "trk")
+    |> Seq.map 
+        (fun trk -> 
+            (let name = trk.Element(gpxNameSpace + "name")
+             if name <> null then name.Value else ""), 
+            trk.Descendants(gpxNameSpace + "trkpt")
+            |> Seq.map 
+                (fun trkpt -> 
+                    (trkpt.Attribute(XName.Get("lat")).Value |> double),
+                    (trkpt.Attribute(XName.Get("lon")).Value |> double), 
+                    (   
+                        let ele = trkpt.Element(gpxNameSpace + "ele")
+                        if ele <> null then ele.Value |> double else Double.NaN
+                    ), 
+                    (   
+                        let time = trkpt.Element(gpxNameSpace + "time")
+                        if time <> null then DateTime.Parse(time.Value) else DateTime.MinValue
+                    )
+                )
+            |> Seq.toList
+        )
+    |> Seq.toList
+
+let gpxRoutes content = 
+    let gpxDoc = gpxDoc content
+    gpxDoc.Descendants(gpxNameSpace + "rte")
+    |> Seq.map 
+        (fun rte -> 
+            (let name = rte.Element(gpxNameSpace + "name")
+             if name <> null then name.Value else ""), 
+            rte.Descendants(gpxNameSpace + "rtept")
+            |> Seq.map 
+                (fun rtept -> 
+                    (rtept.Attribute(XName.Get("lat")).Value |> double),
+                    (rtept.Attribute(XName.Get("lon")).Value |> double)
+                )
+            |> Seq.toList
+        )
+    |> Seq.toList
+
